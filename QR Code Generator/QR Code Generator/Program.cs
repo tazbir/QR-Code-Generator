e1d05cbe498ae71692e879a79d15ccc83190ca05
Tazbir <tazbir@brainstation-23.com>
2024-05-21 15:39:02 +0600

    program.cs updated

---
+++ QR Code Generator/QR Code Generator/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using QR_Code_Generator.Generator;
+
+Generator generator = new Generator();
+generator.GenerateQrCode("Hello, World!");
